94a9697818b2f8f6cfc27536c32cd56325233e69
Serafim Sazanov <serafaz@yandex.ru>
2022-12-01 15:28:31 +0300

    second commit

--- main.py
+++ main.py
@@ -3,5 +3,10 @@ def main_func():
     print('and ex it every day')
 
 
+def sec_foo():
+    print('my tmate is probably top1 of europe on shadowFiend*!*!*!*!*')
+
+
 if __name__ == '__main__':
     main_func()
+    sec_foo()
